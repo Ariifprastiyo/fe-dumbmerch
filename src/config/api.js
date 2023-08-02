099d8e76d631fb8b8de3d8e7155bcc93816e6d3b
Ariifprastiyo <118162340+Ariifprastiyo@users.noreply.github.com>
2023-08-03 06:43:02 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -5,7 +5,7 @@ export const API = axios.create({
   baseURL: process.env.REACT_APP_BASEURL,
 });
 
-// Set Authorization Token Header oke
+// Set Authorization Token Header
 export const setAuthToken = (token) => {
   if (token) {
     API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
